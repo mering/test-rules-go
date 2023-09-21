5002a4457d6af2ee8e21488a558a2f09ef72cce4
maleo <maleo@google.com>
2023-09-21 15:13:27 +0000

    Infer go proto deps automatically

--- src/main.go
+++ src/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	bar_go_proto "src/my_bar_importpath"
-	"src/foo_go_proto"
+	"src/bar_proto"
+	"src/foo_proto"
 )
 
 func main() {
-	foo := &foo_go_proto.Foo{}
-	foo.Bar = &bar_go_proto.Bar{}
+	foo := &foo_proto.Foo{}
+	foo.Bar = &bar_proto.Bar{}
 	foo.Bar.X = 5
 	fmt.Println("Hello world:", foo.Bar.X)
 }
